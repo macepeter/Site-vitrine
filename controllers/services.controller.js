1bf7d303469f453b7a6f9cc7270c034d3b0e0966
macepeter <peter.mace@oclock.school>
2026-01-09 15:33:37 +0100

    finalisation pages services avec card et css

--- controllers/services.controller.js
+++ controllers/services.controller.js
@@ -1,5 +1,12 @@
-
-
 export function servicesRoutes(req, res) {
-    res.render('services');
+    const solutions = [
+    { icon: "💡", title: "Installation d'équipements connectés", desc: "Installation d'appareils intelligents : ampoules, capteurs, caméras…" },
+    { icon: "⚙️", title: "Automatisations personnalisées", desc: "Création de scénarios adaptés à votre quotidien." },
+    { icon: "🌱", title: "Optimisation énergétique", desc: "Réduisez votre consommation grâce à des automatisations intelligentes." },
+    { icon: "🔒", title: "Sécurisation de la maison", desc: "Surveillance, alertes, simulation de présence…" }
+];
+
+    res.render("services", { solutions });
+
 }
+
