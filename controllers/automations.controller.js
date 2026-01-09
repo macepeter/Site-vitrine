ddaffebb26c776ced8da244aee560b05b1a7953d
macepeter <peter.mace@oclock.school>
2026-01-09 16:02:31 +0100

    pages automatisations exemple avec photo et css

--- controllers/automations.controller.js
+++ controllers/automations.controller.js
@@ -1,4 +1,19 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export function automationsPages(req, res) {
-        res.render('automations');
+        
+        const folderPath = path.join(__dirname, "../public/img");
+
+        const images = fs.readdirSync(folderPath)
+                .filter(file => file.match(/\.(jpg|jpeg|png|webp|gif)$/i))
+                .map(file => `/img/${file}`);
+
+        
+        res.render('automations', { images });
 }
 
