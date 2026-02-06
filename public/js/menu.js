cfd98515e2a73b8f14082a4aad6732aefcd167f2
macepeter <peter.mace@oclock.school>
2026-02-06 10:19:36 +0100

    Fix css formulaire de la page A propos

--- public/js/menu.js
+++ public/js/menu.js
@@ -5,3 +5,14 @@ const nav = document.querySelector('.navbar ul');
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault(); 
+
+        alert('Merci pour votre message! Par contre c\'est un site vitrine et je ne peux pas recevoir de messages pour l\'instant. Mes coordonnées sont disponibles dans la section "Contact".'); 
+        form.reset();
+    });
+});
